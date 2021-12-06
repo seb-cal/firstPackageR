03f0743f9171a3d711a044b7b711404dee6569b5
Sebastien Calvignac <sebastien.calvignac@etu.univ-grenoble-alpes.fr>
2021-12-06 17:23:29 +0100

    initial commit golem

---
+++ R/fct_powertwo.R
@@ -0,0 +1,10 @@
+#' powertwo 
+#'
+#' @description A fct function
+#'
+#' @return The return value, if any, from executing the function.
+#'
+#' @noRd
+fct_powertwo <- function(x) {
+  return(x**2)
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(firstPackageR)
+
+test_check("firstPackageR")
